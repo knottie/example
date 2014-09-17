6136501b953ab6b589f8a99e99814d9e6be040aa
Eric Biven <eric@biven.us>
2014-09-17 18:38:18 -0500

    Added linter task to server project.

---
+++ server/gulp/tasks/lint.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+gulp.task('lint', function (cb) {
+    return gulp
+        .src('src/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default', {
+            verbose: true
+        }))
+        //FIXME: This should be failing the build.
+        .pipe(jshint.reporter('fail'));
+});
---
+++ server/src/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+// Insert your code here.
