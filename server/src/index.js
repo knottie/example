ee2172c5560e7a51d9c83aa3b1a4ffb919ce2d71
Eric Biven <eric@biven.us>
2014-09-17 22:34:30 -0500

    Added code coverage with Istanbul.

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,33 @@
 var m = {};
 
 // Insert your code here.
+
+// This function is tested.
 m.sayHello = function sayHello(name) {
   return 'Hello, ' + name;
 };
+
+// This function is not tested, but ignored by Istanbul.
+/* istanbul ignore next */
+m.ignored = function ignored(foo) {
+  if (foo === true) {
+    return true;
+  } else {
+    return false;
+  }
+
+  return false;
+}
+
+// This function is untested.
+m.notCovered = function notCovered(foo) {
+  if (foo === true) {
+    return true;
+  } else {
+    return false;
+  }
+
+  return false;
+}
 
 module.exports = m;
