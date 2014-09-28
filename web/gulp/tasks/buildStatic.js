d04f41cd525ba72642643a2b2386be78df30fcfe
Eric Biven <eric@biven.us>
2014-09-28 17:02:17 -0500

    Fixes #7, add Font Awesome.

--- web/gulp/tasks/buildStatic.js
+++ web/gulp/tasks/buildStatic.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 
-//gulp.task('copyStatic', ['copyBootstrapFonts', 'copyFontAwesomeFonts'], function() {
 gulp.task('buildStatic', function() {
-  return gulp.src('src/static/**')
+  gulp
+    .src('src/static/**')
     .pipe(gulp.dest('build'));
+  gulp
+    .src('node_modules/bootstrap/dist/fonts/**')
+    .pipe(gulp.dest('build/fonts'));
+  gulp
+    .src('node_modules/font-awesome/fonts/**')
+    .pipe(gulp.dest('build/fonts'));
 });
