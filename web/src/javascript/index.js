9b49a4a7715e0f4fae235a557026cd71db09d843
Eric Biven <eric@biven.us>
2014-09-28 16:21:53 -0500

    Fixes #6, added Twitter Bootstrap and updated templates.

---
+++ web/src/javascript/index.js
@@ -0,0 +1,4 @@
+var $ = require('jquery');
+global.jQuery = $; // Thanks for needing a global named "jQuery", Bootstrap.
+
+var bootstrap = require('../../node_modules/bootstrap/dist/js/bootstrap.js');
