d54745baea47283e3d5eb72bcad74cbe69af9992
Eric Biven <eric@biven.us>
2014-10-04 17:30:54 -0500

    Moved testWatch task to test --watch, we'll need args for prod builds.

--- common/gulp/tasks/test.js
+++ common/gulp/tasks/test.js
@@ -1,11 +1,37 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var argv = require('minimist')(process.argv.slice(2));
 
 gulp.task('test', function() {
-  return gulp
-    .src('test/**/*_spec.js', {read: false})
-    .pipe(mocha({
-      reporter: 'spec',
-      ui: 'bdd'
-    }));
+  if (argv.watch) {
+    // Run the tests immediately.
+    gulp
+      .src('test/**/*_spec.js', {read: false})
+      .pipe(mocha({
+        reporter: 'min',
+        ui: 'bdd',
+        growl: true
+      }))
+      .on('error', function() {});
+
+    // Now start the watcher and run them on file changes.
+    return gulp
+      .watch(['src/**',  'test/**'], function() {
+        return gulp
+          .src('test/**/*_spec.js', {read: false})
+          .pipe(mocha({
+            reporter: 'min',
+            ui: 'bdd',
+            growl: true
+          }))
+          .on('error', function() {});
+      });
+  } else {
+    return gulp
+      .src('test/**/*_spec.js', {read: false})
+      .pipe(mocha({
+        reporter: 'spec',
+        ui: 'bdd'
+      }));
+  }
 });
