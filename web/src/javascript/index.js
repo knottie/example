253c02bdcaaa377b9415e011ae3f009aa45c1c46
Eric Biven <eric@biven.us>
2014-10-22 20:24:43 -0500

    wip

--- web/src/javascript/index.js
+++ web/src/javascript/index.js
@@ -2,11 +2,18 @@ var $ = require('jquery');
 global.jQuery = $; // Thanks for needing a global named "jQuery", Bootstrap.
 
 var bootstrap = require('../../node_modules/bootstrap/dist/js/bootstrap.js');
+var ko = require('knockout');
 
 m = {};
 
 m.sum = function sum(x, y) {
   return x + y;
 };
+
+m.viewModel = function viewModel() {
+  this.kobound = ko.observable('Foo!!!');
+};
+
+ko.applyBindings(m.viewModel);
 
 module.exports = m;
